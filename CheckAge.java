d8aa505bb5f5791c6f826f3c00a775cf3a1a6b68
Jayesh Soni <jsoni4502@gmail.com>
2024-04-16 19:15:30 +0530

    New File Added

---
+++ CheckAge.java
@@ -0,0 +1,39 @@
+package tasks;
+import java.util.Scanner;
+
+/**
+ * This Program takes user's age as an input and checks it using if/else statements and displays appropriate message in output
+ * @author Jayesh Soni
+ * @since 2024-04-16
+ *
+ */
+public class CheckAge {
+	public static void main(String[] args) {
+		
+		//Create Scanner Object for user input
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Your Age:");
+		int age = sc.nextInt();
+		
+		//Check age using if/else statements
+		if(age<0)
+		{
+			System.out.println("Invalid Age.");
+		}
+		else if(age<18)
+		{
+			System.out.println("You are minor");
+		}
+		else if(age<60)
+		{
+			System.out.println("You are an adult.");
+		}
+		else
+		{
+			System.out.println("You are a senior citizen");
+		}
+		
+		
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package tasks;
+/**
+ * This program simply displays "Hello World!" output on console.
+ * @author Jayesh Soni
+ * @since 2024-04-16
+ */
+
+public class HelloWorld {
+	public static void main(String [] args)
+	{
+		System.out.print("Hello World!");
+	}
+}
