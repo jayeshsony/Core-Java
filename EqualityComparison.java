2277b24f49e81fa93cfacd0ccaff45286ea12a68
Jayesh Soni <jsoni4502@gmail.com>
2024-04-19 11:11:44 +0530

    Modified the file

--- EqualityComparison.java
+++ EqualityComparison.java
@@ -13,40 +13,40 @@ public class EqualityComparison {
 
 	public static void main(String[] args) {
 		// String Comparison
-		String string1 = "Jayesh";
-		String string2 = "Jayesh";
-		String string3 = new String("Jayesh");
+		String name1 = "Jayesh";
+		String name2 = "Jayesh";
+		String name3 = new String("Jayesh");
 
 		System.out.println("\nString Comparison \nDifference using '==' operator");
-		System.out.println(string1 == string2); // True because the reference is same
-		System.out.println(string1 == string3); // False because the reference is different
+		System.out.println(name1 == name2); // True because the reference is same
+		System.out.println(name1 == name3); // False because the reference is different
 
 		System.out.println("Difference using '.equals()' method");
-		System.out.println(string1.equals(string2)); // True because the content is same
-		System.out.println(string1.equals(string3)); // True because the content is same
+		System.out.println(name1.equals(name2)); // True because the content is same
+		System.out.println(name1.equals(name3)); // True because the content is same
 
 		// Integer Comparison
-		Integer int1 = 150;
-		Integer int2 = 150;
+		Integer value1 = 150;
+		Integer value2 = 150;
 
 		System.out.println("\nInteger Comparison \nDifference using '==' operator");
-		System.out.println(int1 == int2); // False because value is outside the caching range(-128 to 127)
+		System.out.println(value1 == value2); // False because value is outside the caching range(-128 to 127)
 
 		System.out.println("Difference using '.equals()' method");
-		System.out.println(int1.equals(int2)); // True because the content is same
+		System.out.println(value1.equals(value2)); // True because the content is same
 
 		// BigDecimal Comparison
-		BigDecimal bigDecimal1 = new BigDecimal(1.1);
-		BigDecimal bigDecimal2 = new BigDecimal(1.1);
-		BigDecimal bigDecimal3 = new BigDecimal(1.11);
+		BigDecimal decimalValue1 = new BigDecimal(1.1);
+		BigDecimal decimalValue2 = new BigDecimal(1.1);
+		BigDecimal decimalValue3 = new BigDecimal(1.11);
 
 		System.out.println("\nBigDecimal Comparison \nDifference using '==' operator");
-		System.out.println(bigDecimal1 == bigDecimal2); // False because the reference is different
-		System.out.println(bigDecimal1 == bigDecimal3); // False because the reference is different
+		System.out.println(decimalValue1 == decimalValue2); // False because the reference is different
+		System.out.println(decimalValue1 == decimalValue3); // False because the reference is different
 
 		System.out.println("Difference using '.equals()' method");
-		System.out.println(bigDecimal1.equals(bigDecimal2)); // True because the content is same
-		System.out.println(bigDecimal1.equals(bigDecimal3)); // False because the content is different
+		System.out.println(decimalValue1.equals(decimalValue2)); // True because the content is same
+		System.out.println(decimalValue1.equals(decimalValue3)); // False because the content is different
 	}
 
 }
