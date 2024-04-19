8f75dc5871c95e4f73041bec534caf55ce5ba338
Jayesh Soni <jsoni4502@gmail.com>
2024-04-19 10:56:54 +0530

    Removed file

---
+++ StaticFeaturesIllustration.java
@@ -0,0 +1,47 @@
+package tasks;
+
+/**
+ * This program demonstrates the working of static variable, method and block
+ *
+ * @author Jayesh Soni
+ * @since 2024-04-18
+ */
+public class StaticFeaturesIllustration {
+
+	// Initialize static variable
+	static int staticValue = 100;
+
+	// Static block
+	static {
+		System.out.println("Static block initialized");
+		System.out.println("Value of static variable = " + staticValue);
+	}
+
+	/**
+	 * A method of StaticDemo class. It updates the value of static variable.
+	 */
+	public void updateStaticValue() {
+		staticValue += 100;
+	}
+
+	/**
+	 * A method of StaticDemo class.
+	 * This method prints the message indicating static method is initialized.
+	 * It also prints the value of static variable.
+	 */
+	static void printStaticInfo() {
+		System.out.println("Static method initialized");
+		System.out.println("Value of static variable = " + staticValue);
+	}
+
+	public static void main(String[] args) {
+		StaticFeaturesIllustration firstInstance = new StaticFeaturesIllustration();
+		staticDemoObject1.updateStaticValue();
+		printStaticInfo();
+
+		StaticFeaturesIllustration secondInstance = new StaticFeaturesIllustration();
+		staticDemoObject2.updateStaticValue();
+		printStaticInfo();
+	}
+
+}
